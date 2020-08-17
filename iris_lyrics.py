ff270b1ccbfc09a1970ae2a2d0d332d036a2768a
daxjain789 <61685074+daxjain789@users.noreply.github.com>
2020-08-17 15:42:27 +0530

    Add files via upload

---
+++ iris_lyrics.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+print(tf.__version__)
+
+path = r"G:\Coursera Course\Tensorflow-practice\3. NLP in tensorflow\dataset\irish-lyrics.txt"
+
+
+with open(path, 'r') as f:
+    data = f.read()
+
+print(type(data))
+corpus = data.lower().split("\n")
+
+# print(corpus)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(corpus)
+total_words = len(tokenizer.word_index) + 1
+
+print(tokenizer.word_index)
+print(total_words)
+
+input_sequences = []
+for line in corpus:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
+
+# pad sequences
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+
+# create predictors and label
+xs, labels = input_sequences[:,:-1],input_sequences[:,-1]
+
+ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+
+model = Sequential()
+model.add(layers.Embedding(total_words, 64, input_length=max_sequence_len-1))
+model.add(layers.Bidirectional(layers.LSTM(20)))
+model.add(layers.Dense(total_words, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+history = model.fit(xs, ys, epochs=200, verbose=1)
+
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.show()
+
+plot_graphs(history, 'accuracy')
+plot_graphs(history, 'loss')
+
+
+
+# test the model
+seed_text = "The sun is red"
+next_words = 100
+
+for _ in range(next_words):
+    token_list = tokenizer.texts_to_sequences([seed_text])[0]
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+    predicted = model.predict_classes(token_list, verbose=0)
+    output_word = ""
+    for word, index in tokenizer.word_index.items():
+        if index == predicted:
+            output_word = word
+            break
+    seed_text += " " + output_word
+print(seed_text)
